ba2ec8b86102f052b55e781cc6f16c6cddf746ae
JASH GUSANI~ <jashgusani20@gmail.com>
2024-09-20 18:45:42 +0530

    Changes

--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,6 @@
-import mongoose ,{Schema , model } from 'mongoose';
-import {hash} from 'bcryptjs';
+import mongoose, { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs'; 
+const { hash } = bcrypt; 
 
 const schema = new Schema({
     name: {
@@ -31,10 +32,11 @@ const schema = new Schema({
     timestamps: true
 });
 
-schema.pre("save" , async function(next){
-    if(!this.isModified("password")) return next()
-    
-    this.password = await hash(this.password,10); 
-})
+schema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
-export const User = mongoose.models.User || model("User" , schema)
+    this.password = await hash(this.password, 10);
+    next(); // Call next() to proceed
+});
+
+export const User = mongoose.models.User || model("User", schema);
